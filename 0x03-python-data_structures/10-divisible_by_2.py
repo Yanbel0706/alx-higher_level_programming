17c633527627fba8fe44b284a61a42c62223bc41
yanbel0706 <yanis.belk.edu@gmail.com>
2023-12-04 14:20:51 +0100

    all great

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    mult = []
+    num_list = []
     for i in range(len(my_list)):
         if (my_list[i] % 2) == 0:
-            mult.append(True)
-    else:
-            mult.append(False)
-    return mult
+            num_list.append(True)
+        else:
+            num_list.append(False)
+    return num_list
