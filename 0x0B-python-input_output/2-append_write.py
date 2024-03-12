a4f896ccf4b9c91c96045f391282909840f7a0f0
yanbel0706 <yanis.belk.edu@gmail.com>
2024-03-12 17:34:29 +0100

    changing the name of fuction

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """defines a file-appending function"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """Appends a string to the end of a UTF8 text file.
     Args:
         filename (str): The name of the file to append to.
